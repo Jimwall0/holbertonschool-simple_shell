1163d804d11cdf877b9ed801a29fc0be5a6e5810
Your Name <you@example.com>
2023-12-06 12:28:10 -0800

    modified findpath program, returns pointer to path name

--- findpath.c
+++ findpath.c
@@ -1,54 +1,21 @@
-#include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include "shell.h"
 
-char *find_executable(const char *command, char *env[])
+char *findpath(char **env)
 {
-	char *path_var = NULL;
-	char *path_copy;
-	char *dir;
-	char full_path[MAX_PATH_LEN];
-	int i;
+	int i = 0;
 
-	for (i = 0; env[i] != NULL; i++)
+	while (env[i] != NULL)
 	{
 		if (strncmp(env[i], "PATH=", 5) == 0)
-			/* checks first 5 characters for match */
 		{
-		path_var = env[i] + 5; /* Skip "PATH=" */
-		break;
+			return (strdup(env[i] + 5));
 		}
+		i++;
 	}
-	if (path_var == NULL)
-	{
-		fprintf(stderr, "Error: PATH variable not set.\n");
-		return (NULL);
-	}
-	path_copy = strdup(path_var);
-	/* creates copy of path var that can be manipulated */
-	if (path_copy == NULL)
-	{
-		perror("Error");
-		return (NULL);
-	}
-	dir = strtok(path_copy, ":");
-	/* splits path into individual directories */
-	while (dir != NULL)
-	{
-		strcpy(full_path, dir);
-		strcat(full_path, "/");
-		strcat(full_path, command);
-		/* builds full path by combining directory and command */
-		if (access(full_path, X_OK) == 0)
-			/* checks if file is executable */
-		{
-			free(path_copy);
-			return (strdup(full_path));
-		}
-		dir = strtok(NULL, ":");
-	}
-	free(path_copy);
-	return (NULL);
+
+	return (0);
 }
