df66a0a8372d25a56df684ada5d840a24dc08983
Tsim <tyang4204@gmail.com>
2023-12-07 09:50:31 -0800

    added environg back

--- shell.h
+++ shell.h
@@ -1,10 +1,15 @@
 #ifndef SHELL_H
 #define SHELL_H
-
+extern char **environ;
 #define MAX_PATH_LEN 256
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 char* find_executable(const char *command, char *env[]);
-int main(int argc, char **argv, char **env);
-
+int main(int argc, char **argv);
 
 #endif
