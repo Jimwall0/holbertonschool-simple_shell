357b88197f63aa30b44b8ccbd53e37ed397aacbc
Your Name <you@example.com>
2023-12-11 12:11:53 -0800

    changed execve to not fork if command not found

--- execve.c
+++ execve.c
@@ -17,57 +17,59 @@ void forkandexec(char **pathtok, char **tokens)
 	char temp_path[256];
 	size_t len;
 
-	cpid = fork();
-	if (cpid == -1)
-	{
-		perror("Forking Error");
-		exit(1);
-	}
-	else if (cpid == 0)
-	{
-		while (pathtok[i] != NULL)
-		{
-			len = strlen(pathtok[i]);
-			if (tokens[0][0] == '/' && access(tokens[0], X_OK) == 0)
-				/* if input starts with /, execute with absolute path */
-			{
-				execve(tokens[0], tokens, environ);
-				perror("Execution error");
-				exit(2);
-			}
-			if (len > 0 && pathtok[i][len - 1] != '/')
-			{
-				strncpy(temp_path, pathtok[i], sizeof(temp_path) - 1); /* copies path */
-				strncat(temp_path, "/", sizeof(temp_path) - len - 1);
-				/* only adds / if not there already */
-			}
-			else
-			{
-				strncpy(temp_path, pathtok[i], sizeof(temp_path) - 1); /* copies path */
-			}
-			temp_path[sizeof(temp_path) - 1] = '\0'; /* null terminates */
-			strncat(temp_path, tokens[0], sizeof(temp_path) - strlen(temp_path) - 1); 
+	while (pathtok[i] != NULL)
+                {
+                        len = strlen(pathtok[i]);
+                        if (tokens[0][0] == '/' && access(tokens[0], X_OK) == 0)
+                                /* if input starts with /, execute with absolute path */
+                        {
+                                execve(tokens[0], tokens, environ);
+                                perror("Execution error");
+                                exit(2);
+                        }
+                        if (len > 0 && pathtok[i][len - 1] != '/')
+                        {
+                                strncpy(temp_path, pathtok[i], sizeof(temp_path) - 1); /* copies path */
+                                strncat(temp_path, "/", sizeof(temp_path) - len - 1);
+                                /* only adds / if not there already */
+                        }
+                        else
+                        {
+                                strncpy(temp_path, pathtok[i], sizeof(temp_path) - 1); /* copies path */
+                        }
+                        temp_path[sizeof(temp_path) - 1] = '\0'; /* null terminates */
+                        strncat(temp_path, tokens[0], sizeof(temp_path) - strlen(temp_path) - 1);
 			/* command added to end of path */
+
 			if (access(temp_path, X_OK) == 0) /* checks for executable permission */
 			{
-				execve(temp_path, tokens, environ);
-				perror("Execution error");
-				exit(2); /*in case of execve failure */
+				cpid = fork();
+				if (cpid == -1)
+				{
+					perror("Forking Error");
+					exit(1);
+				}
+				else if (cpid == 0)
+				{
+					execve(temp_path, tokens, environ);
+					perror("Execution error");
+					exit(2);
+				}
+				else
+				{
+					waitpid(cpid, &status, 0);
+					if (WEXITSTATUS(status))
+					{
+						exit(WEXITSTATUS(status));
+					}
+					return; /* exit function if command not found */
+				}
 			}
-			i++; /* increments to next element in path array */
-		}
-		fprintf(stderr, "Command not found: %s\n", tokens[0]);
-		free_array(tokens);
-		free_array(pathtok);
-		exit(2);
-	}
-	else
-	{
-		waitpid(cpid, &status, 0);
-		if (WEXITSTATUS(status))
-		{
-			exit(WEXITSTATUS(status));
-		}
-	}
-}
 
+			i++;
+		}
+	fprintf(stderr, "Command not found: %s\n", tokens[0]);
+	free_array(tokens);
+	free_array(pathtok);
+	exit(2);
+}
